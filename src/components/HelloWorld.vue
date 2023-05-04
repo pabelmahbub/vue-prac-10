9a1d99d00a24eecc22d867df375a859df3bc27cc
Mahbub <pabelmahbub@gmail.com>
2023-05-05 04:27:01 +0900

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import ModalView from './ModalView.vue';
+import { ref } from 'vue';
+
+const modalActive = ref(null);
+const data = []
+const toggleModal = () =>{
+  modalActive.value = !modalActive.value
+}
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(json => console.log(json))
+
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+   <h2>HHHH</h2>
+   <button @click="toggleModal">Open Modal</button>
+  </div>
+
+  <ModalView :modalActive = modalActive @close-modal="toggleModal">
+   <h2>This is modal</h2>
+   <p>Lorem Ipsum</p>
+   <h2>This is modal</h2>
+   <p>Lorem Ipsum</p>
+
+
+  </ModalView>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+
+</style>
---
+++ src/components/ModalView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div v-show="modalActive" style="width:300px;height:300px;position:absolute; background-color: rgb(10, 77, 247);">
+       <h2>This Is modal</h2>
+       <slot />
+
+       <button @click="$emit('close-modal')"> Close Me</button>
+    </div>
+</template>
+
+<script setup>
+defineEmits(['close-modal'])
+defineProps({
+    modalActive: {
+        type: Boolean,
+        default: false
+    }
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
